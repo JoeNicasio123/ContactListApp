b1519968056f960ac15614bfe92fad40b1639ef2
JoeNicasio123 <joenicasio123@gmail.com>
2025-03-30 18:14:44 -0500

    Initial Commit

---
+++ My Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Contact List
+//
+//  Created by user272075 on 3/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
