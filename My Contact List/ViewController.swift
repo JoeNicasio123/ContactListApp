11e6374a2c49b7ae6c88dcd8a3af1007b6f00796
JoeNicasio123 <joenicasio123@gmail.com>
2025-04-02 16:13:51 -0500

    keyboard no hides when user scrolls

--- My Contact List/ViewController.swift
+++ My Contact List/ViewController.swift
@@ -9,6 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterKeyboardNotifications()
+    }
+    
+    func registerKeyboardNotifications() {
+            NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardDidShow(notification:)), name:
+                UIResponder.keyboardDidShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector:
+                #selector(ViewController.keyboardWillHide(notification:)), name:
+                UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func unregisterKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+        
+    @objc func keyboardDidShow(notification: NSNotification) {
+        let userInfo: NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardInfo = userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue
+        let keyboardSize = keyboardInfo.cgRectValue.size
+            
+        // Get the existing contentInset for the scrollView and set the bottom property to
+        //be the height of the keyboard
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = keyboardSize.height
+            
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = contentInset
+    }
+        
+    @objc func keyboardWillHide(notification: NSNotification) {
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = 0
+        
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
