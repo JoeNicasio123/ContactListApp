cfdd5c3cc1502b6beff5703cf4d076dfbff32c9d
JoeNicasio123 <joenicasio123@gmail.com>
2025-04-06 17:24:02 -0500

    picker view prints in console

--- My Contact List/SettingsViewController.swift
+++ My Contact List/SettingsViewController.swift
@@ -7,12 +7,39 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var swAscending: UISwitch!
+    @IBOutlet weak var pckSortField: UIPickerView!
+    
+    let sortOrderItems: Array<String> = ["ContactName", "City", "Birthday"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pckSortField.dataSource = self;
+        pckSortField.delegate = self;
+    }
+    
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickeView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return sortOrderItems.count
+    }
+    
+    func pickerView(_ pickeView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sortOrderItems[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Chosen item: \(sortOrderItems[row])")
     }
     
 
