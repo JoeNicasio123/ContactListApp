3060d7070caf98380d09e01507d5cd85a7cd2c89
JoeNicasio123 <joenicasio123@gmail.com>
2025-04-14 13:45:16 -0500

    main screen now display date

---
+++ My Contact List/DateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DateViewController.swift
+//  My Contact List
+//
+//  Created by user272075 on 4/14/25.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
