5e3149c5dd3477099b4db3bac03de8efeb1ef05b
JoeNicasio123 <joenicasio123@gmail.com>
2025-04-21 16:24:13 -0500

    handling live location updates

--- My Contact List/LocationDemoViewController.swift
+++ My Contact List/LocationDemoViewController.swift
@@ -77,9 +77,64 @@ class LocationDemoViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func deviceCoordinates(_ sender: Any) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
     }
-
-    /*
+    
+    override func viewDidAppear(_ animated: Bool) {
+        locationManager.stopUpdatingLocation()
+        locationManager.stopUpdatingHeading()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let eventDate = location.timestamp
+            let howRecent = eventDate.timeIntervalSinceNow
+            if Double(howRecent) < 15.0 {
+                let coordinate = location.coordinate
+                lblLongitude.text = String(format: "%g\u{00B0}", coordinate.longitude)
+                lblLatitude.text = String(format: "%g\u{00B0}", coordinate.latitude)
+                lblLocationAccuracy.text = String(format: "%gm", location.horizontalAccuracy)
+                lblAltitude.text = String(format: "%gm", location.altitude)
+                lblAltitudeAccuracy.text = String(format: "%gm", location.verticalAccuracy)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+           if newHeading.headingAccuracy > 0 {
+               let theHeading = newHeading.trueHeading
+               var direction: String
+               switch theHeading {
+               case 225..<315:
+                   direction = "W"
+               case 135..<225:
+                   direction = "S"
+               case 45..<135:
+                   direction = "E"
+               default:
+                   direction = "N"
+               }
+               lblHeading.text = String(format: "%g\u{00B0} (%@)", theHeading, direction)
+               lblHeadingAccuracy.text = String(format: "%g\u{00B0}", newHeading.headingAccuracy)
+           }
+       }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+          let errorType = error._code == CLError.denied.rawValue ? "Location Permission Denied" :
+          "Unknown Error"
+          let alertController = UIAlertController(title: "Error Getting Location: \(errorType)",
+              message: "Error Message: \(error.localizedDescription))",
+              preferredStyle: .alert)
+          let actionOK = UIAlertAction(title: "OK",
+                                       style: .default,
+                                       handler: nil)
+          
+          alertController.addAction(actionOK)
+          present(alertController, animated: true, completion: nil)
+      }    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
