20ee4e1785718dde293c98667af9cac05ffbdcbd
JoeNicasio123 <joenicasio123@gmail.com>
2025-04-21 18:17:03 -0500

    app now zooms on contacts when find me is pressed

--- My Contact List/MapsViewController.swift
+++ My Contact List/MapsViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 import MapKit
+import CoreData
 
-class MapsViewController: UIViewController, CLLocationManagerDelegate {
+class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     var locationManager: CLLocationManager!
     @IBOutlet weak var mapView: MKMapView!
+    var contacts:[Contact] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +24,68 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         
+        mapView.delegate = self
+        
+    }
+    
+    func mapView(_mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        var span = MKCoordinateSpan()
+        span.latitudeDelta = 0.2
+        span.longitudeDelta = 0.2
+        let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, span: span)
+        mapView.setRegion(viewRegion, animated: true)
     }
     
     @IBAction func findUser(_ sender: Any) {
-        mapView.showsUserLocation = true
-        mapView.setUserTrackingMode(.follow, animated: true)
+        mapView.showAnnotations(mapView.annotations, animated: true)
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+            //Get contacts from Core Data
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let request = NSFetchRequest<NSManagedObject>(entityName: "Contact")
+            var fetchedObjects:[NSManagedObject] = []
+            do {
+                fetchedObjects = try context.fetch(request)
+            } catch let error as NSError {
+                print("Could not fetch. \(error), \(error.userInfo)")
+            }
+            contacts = fetchedObjects as! [Contact]
+            //remove all annotations
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            //go through all contacts
+            for contact in contacts { //as! [Contact] {
+                let address = "\(contact.streetAddress!), \(contact.city!) \(contact.state!)"
+                //geocoding
+                let geoCoder = CLGeocoder()
+                geoCoder.geocodeAddressString(address) {(placemarks, error) in
+                    self.processAddressResponse(contact, withPlacemarks: placemarks, error: error)
+                }
+            }
+        }
+        
+        private func processAddressResponse(_ contact: Contact, withPlacemarks placemarks: [CLPlacemark]?,
+                                            error: Error?) {
+            if let error = error {
+                print("Geocode Error: \(error)")
+            }
+            else {
+                var bestMatch: CLLocation?
+                if let placemarks = placemarks, placemarks.count > 0 {
+                    bestMatch = placemarks.first?.location
+                }
+                if let coordinate = bestMatch?.coordinate {
+                    let mp = MapPoint(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    mp.title = contact.contactName
+                    mp.subtitle = contact.streetAddress
+                    mapView.addAnnotation(mp)
+                }
+                else {
+                    print("Didn't find any matching locations")
+                }
+            }
+        }
     /*
     // MARK: - Navigation
 
